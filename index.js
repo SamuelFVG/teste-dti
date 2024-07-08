2a57421a566305f916af91cc50bd05370a0a0de6
SamuelFVG <samuelfelipeif@gmail.com>
2024-07-08 14:45:50 -0300

    feat: replace sorting function and add test cases

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-const nums1 = [1, 2, 3, 0, 0, 0];
-const m = 3;
+function getSortedArray(nums1, m, nums2, n) {
+  let i = 0,
+    j = 0;
+  const newArray = [];
 
-const nums2 = [2, 5, 6];
-const n = 3;
-
-const newArray = [];
-
-for (let i = 0; i < m; i++) newArray.push(nums1[i]);
-for (let i = 0; i < n; i++) newArray.push(nums2[i]);
-
-for (let i = 0; i < m + n; i++) {
-  for (let j = 1; j < m + n - i; i++) {
-    if (newArray[i] > newArray[j]) {
-      const previousValue = newArray[i];
-      newArray[i] = newArray[j];
-      newArray[j] = previousValue;
+  while (i < m && j < n) {
+    if (nums1[i] < nums2[j]) {
+      newArray.push(nums1[i++]);
+    } else {
+      newArray.push(nums2[j++]);
     }
   }
+
+  while (i < m) {
+    newArray.push(nums1[i++]);
+  }
+
+  while (j < n) {
+    newArray.push(nums2[j++]);
+  }
+
+  console.log(newArray);
 }
 
-console.log(newArray);
+const tests = [
+  { nums1: [1, 2, 3, 0, 0, 0], m: 3, nums2: [2, 5, 6], n: 3 },
+  { nums1: [4, 5, 6, 0, 0, 0], m: 3, nums2: [1, 2, 3], n: 3 },
+  { nums1: [1, 0], m: 1, nums2: [2], n: 1 },
+  { nums1: [0], m: 0, nums2: [1], n: 1 },
+  { nums1: [2, 0], m: 1, nums2: [1], n: 1 },
+  { nums1: [1, 3, 5, 7, 0, 0, 0, 0], m: 4, nums2: [2, 4, 6, 8], n: 4 },
+  { nums1: [1, 2, 4, 5, 6, 0, 0, 0], m: 5, nums2: [3, 7, 8], n: 3 },
+  { nums1: [4, 0, 0, 0, 0, 0], m: 1, nums2: [1, 2, 3, 5, 6], n: 5 },
+  { nums1: [0, 0, 0, 0, 0, 0], m: 0, nums2: [1, 2, 3, 4, 5, 6], n: 6 },
+  { nums1: [1, 2, 3, 4, 5, 6], m: 6, nums2: [], n: 0 },
+  { nums1: [], m: 0, nums2: [0], n: 1 },
+];
+
+tests.forEach(({ nums1, m, nums2, n }) => {
+  getSortedArray(nums1, m, nums2, n);
+});
